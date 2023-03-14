d650d17a3e7dff40cb68aff245fb84683eee3fe8
gabe-dubose <gabe.dubose.sci@gmail.com>
2023-03-14 16:25:51 -0400

    Re-structured CLI

--- quantula/cli/cli.py
+++ quantula/cli/cli.py
@@ -1,76 +1,56 @@
 #!/usr/bin/env python3
 
 import argparse
+from quantula.cli import help_messages
 
 #primary parser
 main_parser = argparse.ArgumentParser(add_help=False)
 main_parser._positionals.title = "Quantula commands"
 main_parser.add_argument('--help', required=False, action='store_true')
-
 #command subparser
 command_subparser = main_parser.add_subparsers(dest="command")
+
+#### DATA-MANAGER COMMAND ####
+
+#initialize data-manager command, subparser, and help option
 data_manager_command = command_subparser.add_parser('data-manager', add_help=False)
-image_adjuster_command = command_subparser.add_parser('image-adjuster', add_help=False)
-color_segmenter_command = command_subparser.add_parser('color-segmenter', add_help=False)
-color_quantifier_command = command_subparser.add_parser('color-quantifier', add_help=False)
-
-#data manager subparser
 data_manager_parser = data_manager_command.add_subparsers(dest="data_manager")
-import_data_command = data_manager_parser.add_parser('import-data', add_help=False)
-export_images_command = data_manager_parser.add_parser('export-images', add_help=False)
-export_quantifications_command = data_manager_parser.add_parser('export-quantifications', add_help=False)
+data_manager_command.add_argument('--help', action='store_true')
 
-#import data subparser
+#data-manager:import-data command and subparser
+import_data_command = data_manager_parser.add_parser('import-data', add_help=False)
 import_data_command.add_subparsers(dest="data_manager_import_data")
+#data-manager: import data commands
 import_data_command.add_argument('--help', action='store_true')
 import_data_command.add_argument('--image-directory', dest="import_images_directory")
 import_data_command.add_argument('--metadata', dest="import_images_metadata")
 import_data_command.add_argument('--output-name', dest="import_images_output_name")
 import_data_command.add_argument('--output-directory', dest="import_data_out_dir")
 
+#data-manager:export-images command and subparser
+export_images_command = data_manager_parser.add_parser('export-images', add_help=False)
+export_images_command.add_subparsers(dest="data_manager_export_images")
+
+#data-manager:export-quantifications command and subparser
+export_quantifications_command = data_manager_parser.add_parser('export-quantifications', add_help=False)
+export_quantifications_command.add_subparsers(dest="data_manager_export_quantifications")
+
+
+#parse arguments
 args = main_parser.parse_args()
 
-#help messages
-main_help_message = '''
-Quantula:
+#print help options
+if args.command == None and args.help == True:
+    print(help_messages.main_help_message)
 
-Commands:
-data-manager\t\tUsage: import and export data
-image-adjuster\t\tUsage: standard image color adjustments
-color-segmenter\t\tUsage: color seqmentation and image re-coloring
-color-quantifier\tUsage: color quantification
+try:
+    #data-manager help options
+    if args.data_manager == None or args.data_manager != None and args.help == True:
+        print(help_messages.data_manager_help)
 
-To see options for specific commands, run:
-Quantula {command} --help
-'''
+    #import-data command help message
+    elif args.data_manager_import_data == None or args.data_manager_import_data != None and args.help == True:
+        print(help_messages.import_data_help) 
+except:
+    pass
 
-data_manager_help = '''
-Quantula data-manager:
-
-Commands:
-import-data\t\tUsage: import images and metadata into a Quantula compressed directorye (qcd)
-export-images\t\tUsage: export images from a qcd file into a standard folder
-export-quantifications\tUsage: export quantification table into a CSV file
-'''
-
-import_data_help = '''
-Quantula data-manager import-data:
-
-Required options:
---image-directory\tDirectory containing images to be imported
---metadata\t\tMetadata file in csv format
---output-name\t\tName of output qcd file
---output-directory\tPath to directory for output qcd to be saved
-'''
-
-#Print main help message if no commands are specified or if help flag is specified
-if args.command == None or args.help == True:
-    print(main_help_message)
-
-#Print data manager help message
-elif args.data_manager == None or args.data_manager != None and args.help == True:
-    print(data_manager_help)
-
-#Print import data help message
-elif args.data_manager_import_data == None or args.data_manager_import_data != None and args.help == True:
-    print(import_data_help)
